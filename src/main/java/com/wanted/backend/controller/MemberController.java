50c85735b581c993d5a26f4d14e143856da26ac2
Yang <didtdjwns111@gmail.com>
2023-08-16 01:35:02 +0900

    feat : Annotation Valid

--- src/main/java/com/wanted/backend/controller/MemberController.java
+++ src/main/java/com/wanted/backend/controller/MemberController.java
@@ -1,9 +1,7 @@
 package com.wanted.backend.controller;
 
 import com.wanted.backend.dto.MemberJoinRequest;
-import com.wanted.backend.dto.MemberJoinResponse;
 import com.wanted.backend.dto.MemberLoginRequest;
-import com.wanted.backend.entity.Member;
 import com.wanted.backend.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/members")
@@ -20,7 +20,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<String> join(@RequestBody MemberJoinRequest request) {
+    public ResponseEntity<String> join(@RequestBody @Valid MemberJoinRequest request) {
 
         memberService.Join(request);
 
@@ -28,7 +28,7 @@ public class MemberController {
     }
     
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody MemberLoginRequest request) {
+    public ResponseEntity<String> login(@RequestBody @Valid MemberLoginRequest request) {
 
         String token = memberService.Login(request);
 
